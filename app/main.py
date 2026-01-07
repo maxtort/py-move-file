3facb99cbfaa932c3cafb20125df09e21602d961
maxtort <topormaks3217@gmail.com>
2026-01-08 00:47:20 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+
+
+def move_file(command: str) -> None:
+    split_command = command.split()
+    if len(split_command) != 3:
+        raise IndexError
+    if split_command[0] != "mv":
+        raise SyntaxError("It`s not command")
+    source, dest_parts = split_command[1], split_command[2]
+    file_name = os.path.basename(source)
+    if dest_parts.endswith(os.sep):
+        target_file_path = os.path.join(dest_parts, file_name)
+    else:
+        target_file_path = dest_parts
+    target_dir = os.path.dirname(target_file_path)
+    if target_dir:
+        os.makedirs(target_dir, exist_ok=True)
+
+    with open(source, "rb") as old, open(target_file_path, "wb") as new:
+        new.write(old.read())
+    os.remove(source)
